451bb16e04987b123d4744a440bef57f7e4dbe6b
maximilianou <maximilianou@gmail.com>
2018-12-21 11:54:52 -0300

    Hello URL routing()

--- server/index.js
+++ server/index.js
@@ -3,21 +3,38 @@ const Hapi = require('hapi');
 class EchoServerHapi{
     constructor(args){
         this.server = Hapi.server(args);
-    };
+    }
     async init(){
         try {
+          await this.routing();
           await this.server.start();
           console.log(`Here we have the echo Server Hapi running:: ${this.server.info.uri}`);
         } catch (error) {
           console.log('Error while trying to run echo Server Hapi:: ' + error.message);
         }
-      };
-      
-};
+    }
+    async routing(){
+        this.server.route({
+            method: 'GET',
+            path: '/',
+            handler: (request, h) => {
+                return 'Hello, Javascript Developer!\n';
+            }
+        });        
+        this.server.route({
+            method: 'GET',
+            path: '/{name}',
+            handler: (request, h) => {
+                return 'Hello, ' + encodeURIComponent(request.params.name) + '!\n';
+            }
+        });
+    }
+}
 
 const firstEchoServer = new EchoServerHapi({
     port: 3000,
     host: 'localhost',
     app: {}
   });
+
 firstEchoServer.init();
